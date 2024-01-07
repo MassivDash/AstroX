414ccf42965a19b641a45106887749317279dfcb
SpaceGhost <lukasz.celitan@gmail.com>
2024-01-07 23:56:46 +0100

    feat(runners): add host, port and env arguments

---
+++ src/runners/production.rs
@@ -0,0 +1,6 @@
+pub fn start_production(host: &str, port: &str, env: &str) {
+    println!("Starting production server");
+    println!("Host: {}", host);
+    println!("Port: {}", port);
+    println!("Env: {}", env);
+}
