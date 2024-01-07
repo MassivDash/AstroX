baddff4dabb3052e104f8419f80e5c6a5d55f2ca
Lukasz Celitan <Lukasz.celitan@eu.jll.com>
2024-01-08 00:33:26 +0100

    build(astro): add wasm for freebsd platform

--- src/frontend/astro.config.mjs
+++ src/frontend/astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { wasm } from '@rollup/plugin-wasm';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        process.platform !== 'freebsd' && wasm()
+    ]
+});
