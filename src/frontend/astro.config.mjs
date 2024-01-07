e6a8cd6a18116cfaaca7bc93955dea83b1841b61
Lukasz Celitan <Lukasz.celitan@eu.jll.com>
2024-01-08 00:42:14 +0100

    fix(astro): add vite plugin for  freebsd platform

--- src/frontend/astro.config.mjs
+++ src/frontend/astro.config.mjs
@@ -4,6 +4,6 @@ import { wasm } from '@rollup/plugin-wasm';
 // https://astro.build/config
 export default defineConfig({
     ...(process.platform === 'freebsd' && { vite: {
-        plugins: [yaml()]
+        plugins: [wasm()]
       }})
 });
