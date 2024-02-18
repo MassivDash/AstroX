ae37cd096d149f9444751e0ddebddd0223e3dc63
Lukasz Celitan <Lukasz.celitan@eu.jll.com>
2024-02-18 18:20:24 +0100

    build: improve linter msg

--- src/runners/linter.rs
+++ src/runners/linter.rs
@@ -1,9 +1,11 @@
 use super::system_checks::NPM;
-use super::terminal::step;
+use super::terminal::{error, spacer, step, warning};
 
 use std::process::Command;
 
 pub fn start_npm_linting() {
+    warning("Running cargo runner bride");
+
     let node_watch = Command::new(NPM)
         .arg("run")
         .arg("lint")
@@ -14,7 +16,13 @@ pub fn start_npm_linting() {
         .expect("Failed to start npm linting");
 
     match node_watch.success() {
-        true => step("NPM linting done successfully"),
-        false => panic!("Linting failed, fix code"),
+        true => {
+            spacer();
+            step("NPM linting done successfully")
+        }
+        false => {
+            error("NPM linting failed");
+            panic!("Linting failed, fix code")
+        }
     }
 }
