3875fa45e79c2a7e45d68f1a3ffbddbdfe1e5bb5
Lukasz Celitan <Lukasz.celitan@eu.jll.com>
2024-02-24 21:51:12 +0100

    test(backend): add routes test

--- src/backend/src/api/hello/get.rs
+++ src/backend/src/api/hello/get.rs
@@ -1,8 +1,25 @@
 use actix_web::{get, HttpResponse, Responder};
+use actix_web::{test, App};
 
 #[get("/api/hello")]
 async fn json_response_get() -> impl Responder {
     HttpResponse::Ok()
         .content_type("application/json")
         .body("{\"message\": \"Hello World\"}")
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[actix_rt::test]
+    async fn test_json_response_get() {
+        let mut app = test::init_service(App::new().service(json_response_get)).await;
+
+        let req = test::TestRequest::get().uri("/api/hello").to_request();
+        let resp = test::call_service(&mut app, req).await;
+
+        assert!(resp.status().is_success());
+
+        let body = test::read_body(resp).await;
+        assert_eq!(body, "{\"message\": \"Hello World\"}");
+    }
 }
