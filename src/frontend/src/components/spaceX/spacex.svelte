13d1e83c9f84a5e0f7443abb2b8b525bbe30546d
Lukasz Celitan <Lukasz.celitan@eu.jll.com>
2024-02-25 13:46:31 +0100

    feat(frontend): hook up simple axios example

---
+++ src/frontend/src/components/spaceX/spacex.svelte
@@ -0,0 +1,65 @@
+<!-- //   {
+    //     "id": 1,
+    //     "active": false,
+    //     "stages": 2,
+    //     "boosters": 0,
+    //     "cost_per_launch": 6700000,
+    //     "success_rate_pct": 40,
+    //     "first_flight": "2006-03-24",
+    //     "country": "Republic of the Marshall Islands",
+    //     "company": "SpaceX",
+    //     "wikipedia": "https://en.wikipedia.org/wiki/Falcon_1",
+    //     "description": "The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.",
+    //     "rocket_id": "falcon1",
+    //     "rocket_name": "Falcon 1",
+    //     "rocket_type": "rocket"
+    //   } -->
+
+<script lang="ts">
+  interface Rocket {
+    id: number
+    active: boolean
+    stages: number
+    boosters: number
+    cost_per_launch: number
+    success_rate_pct: number
+    first_flight: string
+    country: string
+    company: string
+    wikipedia: string
+    description: string
+    rocket_id: string
+    rocket_name: string
+    rocket_type: string
+  }
+
+  import { onMount } from 'svelte'
+  import axios from 'axios'
+
+  const getSpaceXRockets = async () => {
+    try {
+      const res = await axios.get('http://localhost:8080/api/space-x')
+      const data = res.data
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  let data: Rocket[] = []
+
+  onMount(async () => {
+    const res = await getSpaceXRockets()
+    data = res
+  })
+</script>
+
+<div>
+  <p>Svelte</p>
+  {#each data as rocket}
+    <div>
+      <h1>{rocket.rocket_name}</h1>
+      <p>{rocket.description}</p>
+    </div>
+  {/each}
+</div>
