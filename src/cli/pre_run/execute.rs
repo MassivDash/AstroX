f334ab5e3b183698d29d62f313203b63a2ef2ef6
SpaceGhost <lukasz.celitan@gmail.com>
2024-03-10 21:40:10 +0100

    fix(git_hooks): fix condition

--- src/cli/pre_run/execute.rs
+++ src/cli/pre_run/execute.rs
@@ -1,7 +1,7 @@
 use super::{system_checks::run_system_checks, utils::git_hooks};
 
 pub fn execute(env: &str) {
-    if git_hooks::check_if_git_hooks_are_installed() {
+    if !git_hooks::check_if_git_hooks_are_installed() {
         git_hooks::copy_git_hooks();
     }
 
