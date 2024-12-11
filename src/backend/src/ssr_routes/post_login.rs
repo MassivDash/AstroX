5e4df1288cb394b97db9f5c681ce3ecd831c516a
SpaceGhost <lukasz.celitan@gmail.com>
2024-12-11 17:09:23 +0100

    build(login): get rid of ?

--- src/backend/src/ssr_routes/post_login.rs
+++ src/backend/src/ssr_routes/post_login.rs
@@ -28,14 +28,17 @@ pub async fn post_login(
         Ok(user) => {
             println!("User authenticated: {:?}", user);
             session.renew();
-            session.insert("user_id", user.id).map_err(|e| {
-                let error_message = format!("Failed to insert user_id into session: {}", e);
-                login_redirect(LoginError::UnexpectedError(anyhow::anyhow!(error_message)))
-            })?;
-
-            Ok(HttpResponse::SeeOther()
-                .insert_header((LOCATION, "/auth/auth"))
-                .finish())
+            match session.insert("user_id", user.id) {
+                Ok(_) => Ok(HttpResponse::SeeOther()
+                    .insert_header((LOCATION, "/auth/auth"))
+                    .finish()),
+                Err(e) => {
+                    let error_message = format!("Failed to insert user_id into session: {}", e);
+                    Err(login_redirect(LoginError::UnexpectedError(
+                        anyhow::anyhow!(error_message),
+                    )))
+                }
+            }
         }
         Err(e) => {
             let e = match e {
