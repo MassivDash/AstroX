aad4bad6f9f286361ab2a37a6c8be658f2728c91
SpaceGhost <lukasz.celitan@gmail.com>
2026-09-13 11:59:59 +0200

    fix(frontend): update vitest config for vitest 4 and remove deprecated workspace file

--- src/frontend/vitest.config.ts
+++ src/frontend/vitest.config.ts
@@ -1,11 +1,34 @@
 import { getViteConfig } from 'astro/config'
+import { svelteTesting } from '@testing-library/svelte/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default getViteConfig({
+  //@ts-ignore
   test: {
+    projects: [
+      {
+        plugins: [svelteTesting(), tsconfigPaths()],
+        extends: true,
+        test: {
+          setupFiles: './setup.vitest.ts',
+          name: 'browser',
+          include: ['**/*.browser.test.{ts,tsx,jsx,svelte}'],
+          environment: 'js-dom'
+        }
+      },
+      {
+        extends: true,
+        plugins: [tsconfigPaths()],
+        test: {
+          name: 'Astro ssr',
+          include: ['**/!(*.browser).test.{ts,js,tsx,jsx,astro}'],
+          environment: 'node'
+        }
+      }
+    ],
     globals: true,
-    setupFiles: './setup.vitest.ts',
     coverage: {
-      extension: ['.astro', '.ts', '.tsx', '.svelte'],
+      include: ['.astro', '.ts', '.tsx', '.svelte'],
       provider: 'v8',
       reporter: ['text', 'json-summary', 'json', 'cobertura']
     }
